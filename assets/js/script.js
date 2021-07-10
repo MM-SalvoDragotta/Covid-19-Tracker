7357f114f26cc175f796c98a76f56b8b7bf9e64d
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-07-10 10:21:34 +1000

    Add JS function to render chart

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,36 +8,88 @@ const options = {
 	},
   };
 
-var country = "australia"
-const endpointUrl = "https://covid-19-data.p.rapidapi.com/country?name=" + country;
+var countryName = document.getElementById('results-text');
 
+var country = "USA";
+const labels = ['Confirmed', 'Deaths', 'Recovered'];
+countryName.textContent = toTitleCas(country);
 
+const endpointUrl = `https://covid-19-data.p.rapidapi.com/country?name=${country}`;
+
+//https://stackoverflow.com/questions/32589197/how-can-i-capitalize-the-first-letter-of-each-word-in-a-string-using-javascript
+function toTitleCas (phrase) {
+	return phrase
+	  .toLowerCase()
+	  .split(' ')
+	  .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+	  .join(' ');
+  };
+
+//Fetch country data and display it
 fetch(endpointUrl, options)
 	.then(response => {
 		return response.json();
 	  })
 	  .then((body) => {
-		console.log(body);
-	console.log(body[0].confirmed)
+	// console.log(body);
+	// console.log(body[0].confirmed)
+	renderChart(body[0].confirmed, body[0].deaths, body[0].recovered)	
 	  })
 	  .catch((err) => {
 		console.log(err);
 	  });
 
 //helpful tute on how to use charts.js: https://www.youtube.com/watch?v=sE08f4iuOhA 
-let myChart1 = document.getElementById('myChart').getContext('2d');
+function renderChart (confirmed, deaths, recovered ){	
+	// countryData = [30903, 910, 29466]
+	var barChartData ={
+		type: 'bar', //bar, horizontalBar, pie, line, donut, radar, polarArea , doughnut
+			data: {
+			labels: labels,
+			datasets: [{
+			data: [confirmed, deaths, recovered],
+			backgroundColor: ["blue" , "red", "green"],
+			borderColor: ["black" , "black" , "black" ],
+			borderWidth: 1.5,
+			hoverOffset: 4
+		}]		 
+		},
+			options: {
+				// events: ['mousemove', 'mouseout', 'click', 'touchstart', 'touchmove'],
+				plugins: {
+					legend: {
+						display: false
+					},
+					subtitle: {
+						display: true,
+						text: 'Confirmed Deaths Recovered'
+					},
+					// customPlugin : {
+					// 	id: 'custom_canvas_background_color',
+					// 	beforeDraw: (myChart) => {
+					// 		const ctx = myChart.canvas.getContext('2d');
+					// 		ctx.save();
+					// 		ctx.globalCompositeOperation = 'destination-over';
+					// 		ctx.fillStyle = 'black';
+					// 		ctx.fillRect(0, 0, myChart.width, myChart.height);
+					// 		ctx.restore();
+					// 	}
 
-	  let Population = new Chart(myChart, {
-		type: 'line', //bar, horizontalBar, pie, line, donut, radar, polarArea
-		data: {
-		 labels: ['lane cove', 'longuevile', 'crows-nest', 'greenwich'],
-		 datasets: [{
-		   label: 'Population',
-		   data: 
-		   [1934920, 
-			2392395, 
-			12849, 
-			2911204]
-		 }]
+				// },
+				// tooltip: {
+				// 	// Tooltip will only receive click events
+				// 	events: ['click']
+				//   }
+			}
 		}
-	  })
+
+
+	};	
+	var myChart = document.getElementById('myChart').getContext('2d');
+	let covidDataChart = new Chart(myChart, barChartData);
+
+}
+
+
+  
+// add event handler on submit button
