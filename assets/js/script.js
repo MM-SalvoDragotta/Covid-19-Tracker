f0c4e7eddc6f28f238af86af41647d808d35b72f
RoninHietanen <84194778+RoninHietanen@users.noreply.github.com>
2021-07-10 13:54:49 +1000

    added datalist population via api getCountries

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ fetch(endpointUrl, options)
 	  })
 	  .then((body) => {
 		console.log(body);
-	console.log(body[0].confirmed)
+		console.log(body[0].confirmed)
 	  })
 	  .catch((err) => {
 		console.log(err);
@@ -27,17 +27,126 @@ fetch(endpointUrl, options)
 //helpful tute on how to use charts.js: https://www.youtube.com/watch?v=sE08f4iuOhA 
 let myChart1 = document.getElementById('myChart').getContext('2d');
 
-	  let Population = new Chart(myChart, {
+	let Population = new Chart(myChart, {
 		type: 'line', //bar, horizontalBar, pie, line, donut, radar, polarArea
 		data: {
-		 labels: ['lane cove', 'longuevile', 'crows-nest', 'greenwich'],
-		 datasets: [{
+		labels: ['lane cove', 'longuevile', 'crows-nest', 'greenwich'],
+		datasets: [{
 		   label: 'Population',
 		   data: 
 		   [1934920, 
 			2392395, 
 			12849, 
 			2911204]
-		 }]
+		}]
+	}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getCountries() {
+
+	fetch("https://covid-19-data.p.rapidapi.com/help/countries", {
+		method: "GET",
+		headers: {
+			"x-rapidapi-host": rapidApiHost,
+			"x-rapidapi-key": rapidApiKey,
 		}
-	  })
+	})
+	.then(response => {
+		return response.json();
+	})
+	.then((body) => {
+		console.log(body);
+		console.log(body[0].name)
+		for (var i = 0 ; i < body.length ; i++ ) {
+			$("#browsers").append("<option value='" + body[i].name + "'>");
+		};
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+};
+
+getCountries();
