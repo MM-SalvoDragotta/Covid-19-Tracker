a17d43f05719aa1fce0fab22028e5284f3af276e
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-07-15 21:03:25 +1000

    Minor Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,6 @@ const options = {
 
 var countryName = document.getElementById('results-text');
 
-// var country = "USA";
-// var country2 = $('#search-input');
-// console.log(country2.value);
-
 const labels = ['Confirmed', 'Recovered', 'Deaths' ];
 
 var covidDataChart = "";
@@ -53,14 +49,7 @@ function dataByCountry (country){
 	})
 
 	.catch((error) => {
-<<<<<<< HEAD
 		$(".msg" && ".refresh").html ("Please click to refresh and search for a valid country").show()		
-=======
-		console.log(error)
-		console.log(msg)
-		$(".msg" && ".refresh").html ("Please click to refresh and search for a valid country").show()
-
->>>>>>> 5373e480d8ac733b7577151c6f742590a258aa72
 	});
 
 //	msg.textContent = "";
@@ -222,15 +211,8 @@ function getCountries() {
 	})
 
 	.catch((error) => {
-<<<<<<< HEAD
 		$(".msg" && ".refresh").html ("Please click to refresh and search for a valid country").show()
 				
-=======
-		console.log(error);
-
-		$(".msg" && ".refresh").html ("Please click to refresh and search for a valid country").show();		
-
->>>>>>> 5373e480d8ac733b7577151c6f742590a258aa72
 	});
 		
 };
@@ -246,7 +228,3 @@ function refreshPage(){
     window.getElementById(".refresh").reload();
 }   
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 5373e480d8ac733b7577151c6f742590a258aa72
